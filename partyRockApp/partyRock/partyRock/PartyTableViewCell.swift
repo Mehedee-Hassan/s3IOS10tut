35cbd716ff57a9b3dcecd417e99e0ffff082e452
Mehedee Hassan <razin.meha@gmail.com>
2017-01-30 01:58:04 +0600

    add tableview,cell

---
+++ partyRockApp/partyRock/partyRock/PartyTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PartyTableViewCell.swift
+//  partyRock
+//
+//  Created by Mehedee Hassan on 1/30/17.
+//  Copyright © 2017 Mehedee Hassan. All rights reserved.
+//
+
+import UIKit
+
+class PartyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var PartyImage: UIImageView!
+    @IBOutlet weak var VideoTitle: UILabel!
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+ 
+    func updateUI(){
+        
+    }
+    
+    
+    
+}
